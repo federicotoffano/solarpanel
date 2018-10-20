e34ba4154b55b326744f080379fc615f187ef61e
Federico <toffano.federico@gmail.com>
2018-10-20 19:38:34 +0100

    Utils update

--- helloworld.py
+++ helloworld.py
@@ -6,28 +6,36 @@ import Utils
 import LocalPath as lp
 from matplotlib import pyplot as plt
 
-nCols = 6
-nRows = 12
+n_cols = 6
+n_rows = 10
+cell_real_width = 157
+cell_real_height = 157
+cells_real_distance = 2
 
-file = lp.LocalPaths.image_dir_mono + '5.jpg'
-file = lp.LocalPaths.image_dir_mono + '7.jpg'
-file = lp.LocalPaths.image_dir_mono + '9.jpg'
+
+#file = lp.LocalPaths.image_dir_mono + '5.jpg'
+#file = lp.LocalPaths.image_dir_mono + '7.jpg'
+file = lp.LocalPaths.image_dir_mono + '62.jpg'
 
 
 #file = lp.LocalPaths.image_dir_mono + '0208160009_2016-08-03_16-36-24_13659.jpg'
 
-offset_border = 50
-img_grey_org = Utils.crop_img(file, offset_border)
+offset_background = 0
+cropped_img = Utils.crop_img(file, n_rows, n_cols, offset_background)
+Utils.find_cells2(cropped_img, n_rows, n_cols, cell_real_width, cell_real_height, cells_real_distance)
 
-plt.imshow(img_grey_org, cmap='gray', interpolation='bicubic')
+
+
+plt.imshow(cropped_img, cmap='gray', interpolation='bicubic')
 plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
 plt.show()
 
-img_cells = Utils.find_cells(img_grey_org, offset_border, nCols, nRows)
 
-plt.imshow(img_cells, cmap='gray', interpolation='bicubic')
-plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
-plt.show()
+# img_cells = Utils.find_cells(img_grey_org, offset_border, nCols, nRows)
+#
+# plt.imshow(img_cells, cmap='gray', interpolation='bicubic')
+# plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+# plt.show()
 
 
 
